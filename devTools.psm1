94777243e79da1b6e29172035d82019ebef9ba31
Gecko <164238965+Golden1Knight@users.noreply.github.com>
2025-06-24 19:31:39 +0200

    Update devTools.psm1

--- devTools.psm1
+++ devTools.psm1
@@ -1,10 +1,10 @@
 function clear {cls}
 function win-cmd {cmd}
 function m-rand {
-	param(
- 		[int]$mn = 0
-   		[int]$mx = 999999999999
-        )
-	Get-Random -Minimum $mn -Maximum $mx
+param(
+	[int]$mn = 0,
+	[int]$mx = 999999999999
+)
+Get-Random -Minimum $mn -Maximum $mx
 }
 	
